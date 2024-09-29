946dd45a5ed906bb72be6a724f02be04376ede58
Ethan Cao <1677035769@qq.com>
2024-09-29 21:47:26 +0800

    24/9/29 21:47

---
+++ demo.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+source .bashrc
+
+pip install torch torchvision torchaudio
+pip install numpy pandas matplotlib jupyter
+pip install scikit-learn transformers datasets tokenizers tqdm tensorboard torchmetrics
