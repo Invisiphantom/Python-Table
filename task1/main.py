daf3fdb868189077492e0f209504c0913e40d220
Ethan Cao <1677035769@qq.com>
2025-04-05 16:29:37 +0800

    25/4/5 16:29

--- task1/main.py
+++ task1/main.py
@@ -2,11 +2,12 @@ import dill
 import struct
 import numpy as np
 
-eval_datafile_path = ["mnist_testdata/t10k-images.idx3-ubyte", "mnist_testdata/t10k-labels.idx1-ubyte"]
-model_pickle_path = "task1-mnist.pickle"
+# mnist_path = ["/opt/data/mnist_testdata/train-images.idx3-ubyte", "/opt/data/mnist_testdata/train-labels.idx1-ubyte"]
+mnist_path = ["mnist_testdata/t10k-images.idx3-ubyte", "mnist_testdata/t10k-labels.idx1-ubyte"]
+model_path = "task1-mnist.pkl"
 
-with open(model_pickle_path, "rb") as f:
+with open(model_path, "rb") as f:
     model = dill.load(f)
 
-test_accuracy = model.interview(eval_datafile_path)
+test_accuracy = model.interview(mnist_path)
 print(f"测试准确率: {test_accuracy:.2f}%")
