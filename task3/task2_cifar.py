d998ee4e2723c1276e3945464799b398d973f0c9
Ethan Cao <1677035769@qq.com>
2025-04-07 10:37:10 +0800

    25/4/7 10:37

--- task3/task2_cifar.py
+++ task3/task2_cifar.py
@@ -75,27 +75,6 @@ def init_weight(m):
         nn.init.constant_(m.bias, 0)
 
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, stride, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, 3, padding=1),
-            nn.BatchNorm2d(out_channels),
-        )
-
-        # 残差连接匹配维度
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, 1, stride),
-                nn.BatchNorm2d(out_channels),
-            )
-
-    def forward(self, x):
-        return F.relu(self.net(x) + self.shortcut(x))
 
 
 def run(model, loss_fn, initial_lr, batch_size, dataset, device):
